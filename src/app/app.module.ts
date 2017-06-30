562cc18ef123f46ba78f8153098565be32688271
Carlos Eduardo González Cortes <carlosedugoc@gmail.com>
2017-06-29 20:30:24 -0500

    se crean componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,25 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+import { YoutubeService } from './services/youtube.service'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    YoutubeService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
