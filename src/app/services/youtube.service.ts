f9b97ca0b0cec38bd31572ddef5de8a2374e50f5
Carlos Eduardo González Cortes <carlosedugoc@gmail.com>
2017-06-29 20:31:49 -0500

    crea servicio

--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Injectable()
 export class YoutubeService {
+  youtubeUrl:String = "https://www.googleapis.com/youtube/v3/playlistItems";
+  apikey: string = "AIzaSyDJSoCir66vB9mebtlFBP--qaReKSJNFbs"
 
-  constructor() { }
+  constructor(public http:Http) { }
+
+  getVideos(){
+    
+  }
 
 }
